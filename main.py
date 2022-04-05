73c3bd6bd2ee69de752bcabc83293581508ed3cc
mcodexim <101277259+mcodexim@users.noreply.github.com>
2022-04-05 22:22:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
 
-#5165717781:AAFjU1odFXMuyNSnZk4xxoEJTzc2wKUsCTA
 bot = telebot.TeleBot("5165717781:AAFjU1odFXMuyNSnZk4xxoEJTzc2wKUsCTA")
 
 @bot.message_handler(commands=['start', 'help'])
@@ -21,11 +20,9 @@ def notify_step(message):
 	keyboard.add(key_day)
 	bot.send_message(message.from_user.id, text=ed_iden, reply_markup=keyboard)
 
-
 @bot.callback_query_handler(func=lambda call: True)
-def callback_ed(call,text):
-	if call.data=='hour':
-		q1='Выбраны часы. \n Через сколько часов напомнить? \n Или введите число часов самостоятельно.'
+def callback_worker(call):
+	if call.data == 'hour':
 		keyboard = types.InlineKeyboardMarkup()
 		key_1hour = types.InlineKeyboardButton(text='1 час', callback_data='1')
 		keyboard.add(key_1hour)
@@ -33,12 +30,10 @@ def callback_ed(call,text):
 		keyboard.add(key_2hours)
 		key_3hours = types.InlineKeyboardButton(text='3 часа', callback_data='3')
 		keyboard.add(key_3hours)
-		bot.send_message(call.from_user.id, text=q1, reply_markup=keyboard)
-		kolvo_hours=text.message
-		kolvo_hours=int(kolvo_hours)
-
-
-
+		bot.send_message(call.message.chat.id, text="Выбраны часы",reply_markup=keyboard)
+	elif call.data == "min":
+		bot.send_message(call.message.chat.id, "Попробуем еще раз!")
+		bot.send_message(call.message.chat.id, "Привет! Давай познакомимся! Как тебя зовут?")
 
 bot.infinity_polling()
 
