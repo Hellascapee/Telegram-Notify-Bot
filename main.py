5d5f270b6e5f49659658a9f475783b548c4865b0
mcodexim <101277259+mcodexim@users.noreply.github.com>
2022-04-06 13:54:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import telebot
+from telebot import types
+bot = telebot.TeleBot('5165717781:AAFjU1odFXMuyNSnZk4xxoEJTzc2wKUsCTA')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.send_message(message.from_user.id, 'Вас приветствует NotifyBot(бот-напоминание)\n'
+										   'Бот предназначен для создания напоминаний.')
+	bot.send_message(message.from_user.id, 'Чтобы начать введите /notify или /n')
+	bot.register_next_step_handler(message,notify_step)
+def notify_step(message):
+	ed_iden='Выберите единицу времени:'
+	keyboard = types.InlineKeyboardMarkup()
+	key_hour = types.InlineKeyboardButton(text='Часы', callback_data='hour')
+	keyboard.add(key_hour)
+	key_min = types.InlineKeyboardButton(text='Минуты', callback_data='min')
+	keyboard.add(key_min)
+	key_day = types.InlineKeyboardButton(text='Дни', callback_data='days')
+	keyboard.add(key_day)
+	bot.send_message(message.from_user.id, text=ed_iden, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+	if call.data == 'hour':
+		keyboard = types.InlineKeyboardMarkup()
+		key_1hour = types.InlineKeyboardButton(text='1 час', callback_data='1')
+		keyboard.add(key_1hour)
+		key_2hours = types.InlineKeyboardButton(text='2 часа', callback_data='2')
+		keyboard.add(key_2hours)
+		key_3hours = types.InlineKeyboardButton(text='3 часа', callback_data='3')
+		keyboard.add(key_3hours)
+		bot.send_message(call.message.chat.id, text="Выбраны часы",reply_markup=keyboard)
+	elif call.data == "min":
+		keyboard = types.InlineKeyboardMarkup()
+		key_20min = types.InlineKeyboardButton(text='20 минут', callback_data='1')
+		keyboard.add(key_20min)
+		key_30min = types.InlineKeyboardButton(text='30 минут', callback_data='2')
+		keyboard.add(key_30min)
+		key_45min = types.InlineKeyboardButton(text='45 минут', callback_data='3')
+		keyboard.add(key_45min)
+		bot.send_message(call.message.chat.id, text="Выбраны минут", reply_markup=keyboard)
+
+bot.infinity_polling()
